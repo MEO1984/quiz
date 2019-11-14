bc9b9d8ad340953402d0803443518e206fdf2ab1
Mike O'Malley <MEO1984@gmail.com>
2019-11-14 00:41:50 -0500

    update

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 
-var timerDisplay = document.querySelector("timer");
+var timerDisplay = document.querySelector("#timer");
 var highScore = document.querySelector("high");
 var totalSeconds = 0;
 var secondsElapsed = 0;
 var questionCounter = 0;
+var result = document.querySelector("result");
+var secondsLeft = 60;
+var start = document.querySelector("#start")
 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerDisplay.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            $("#main").append('<h1>' + "TIMES UP" + '</h1>') 
+        }
+    }, 1000);
+}
+
+
+
+ 
 
 
 
@@ -28,31 +46,54 @@ var questions = [
             "String"]
         
     },
+    {
+        Question: "What do you use to add a click function?",
+        Answers: [
+            ".on",
+            ".create",
+            ".append",
+            ".math" ]
+    },
+    
+    {        
+        Question: "Which parentheses annotate an array",
+        Answers: [
+            "{}",
+            "[]",
+            "()",
+            "''",]
+
+
+
+    }
 ]
 
-function quesLoop() {
+function quesLoop() { 
+    setTime();
     $('#main').empty().append('<h2>' + questions[questionCounter].Question+ '</h2>').append('<button onclick="ansTest()" data-attribute="0">' + questions[questionCounter].Answers[0] + '</button>').append('<button onclick="ansTest()" data-attribute="1">' + questions[questionCounter].Answers[1] + '</button>').append('<button onclick="ansTest()" data-attribute="2">' + questions[questionCounter].Answers[2] + '</button>').append('<button onclick="ansTest()" data-attribute="3">' + questions[questionCounter].Answers[3] + '</button>');
+    
 
     questionCounter++;
 }
 function ansTest() {
 if ($(this.event.target).attr('data-attribute') === '0') {
-    console.log("Correct!")
+    $("#result").empty().append("Correct!");
+    //I cannot figure out how to call the different indexes within the array to declare answers right or wrong
+
+    
+        
     quesLoop();
-    // To Do: Call function that clears the display and displays the next question
+    
 } else {
-    console.log("Wrong!")
+    $("#result").empty().append("Wrong!")
     // To Do: Call function that removes some time from the timer
 }
-
-function ansTest2() {
-    if ($(questions[1].event.target).attr('data-attribute') === '2') {
-        console.log("Correct!")
-        quesLoop();
-        // To Do: Call function that clears the display and displays the next question
-    } else {
-        console.log("Wrong!")
-        // To Do: Call function that removes some time from the timer
-    }
-    }
 }
+
+
+
+
+
+    
+        
+
